b07fac4dd6e5faa1778838bd7984b86a2e49a87e
Tony Fang <tony46711@yahoo.com>
2024-09-08 10:23:44 -0700

    Add code documentation.

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -1,30 +1,40 @@
+#Homework 1 - CS156
+#Written By: Ziheng (Tony) Fang and Ashwabh Bhatnagar
+
+
 import math
 
 
 def main():
-    adjacencyMatrix = CreateAdjacencyMatrix()
-    pathData = BFAlgorithm(adjacencyMatrix)
+    adjacencyMatrix = CreateAdjacencyMatrix() # this is the adjacency matrix
+    pathData = BFAlgorithm(adjacencyMatrix) # this is the paths to each node from 's'
+    '''This checks to see if a negative cycle exists'''
     if not pathData:
         print("Error, there exists a negative cycle. ")
     else:
         print("No negative cycle exists. ")
+        '''This translates the index into the corresponding vertex.'''
         vertexData = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f'}
+        '''This prints out the shortest distance to each vertex.'''
         for i in range(1, 7):
             print("Shortest distance to " + str(vertexData[i]) + " is: " + str(pathData[0][i]))
+        '''This prints out the path data to each vertex.'''
         for i in range(1, 7):
             print("Path to " + str(vertexData[i]) + " is: " + str(pathData[1][i]))
 
 
 def CreateAdjacencyMatrix():
-    size = 7
+    size = 7 #size is hardcoded
     adjacencyMatrix = []
     for i in range(size):
         adjacencyMatrix.append([])
         for j in range(size):
             adjacencyMatrix[i].append([])
+    '''Start with all infinity. '''
     for i in range(size):
         for j in range(size):
             adjacencyMatrix[i][j] = math.inf
+    '''We then add edge data. '''
     adjacencyMatrix[0][1] = 6
     adjacencyMatrix[0][2] = 5
     adjacencyMatrix[0][3] = 5
@@ -35,6 +45,7 @@ def CreateAdjacencyMatrix():
     adjacencyMatrix[3][5] = -1
     adjacencyMatrix[4][6] = 3
     adjacencyMatrix[5][6] = 3
+    '''Going from a node to itself should not cost anything...'''
     for i in range(7):
         adjacencyMatrix[i][i] = 0
 
@@ -42,23 +53,26 @@ def CreateAdjacencyMatrix():
 
 
 def BFAlgorithm(Matrix):
-    returnData = []
+    returnData = [] # will contain data we return.
     vertexData = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f'}
-    D = []
-    P = []
+    D = [] # contains distances to each vertex.
+    P = [] # contains detailed paths to each vertex
     for i in range(7):
         P.append([])
     for i in range(1, 7):
         if Matrix[0][i] != math.inf:
             P[i].append(vertexData[i])
-    D.append(0)
+    D.append(0) #going from start to itself will not cost anything
     for i in range(1, 7):
         D.append(Matrix[0][i])
-    for i in range(7):
+    '''Loop through the graph V-1 times.'''
+    for i in range(6):
         for j in range(7):
             for k in range(7):
+                '''If we find a shorter path, we relax. '''
                 if D[j] + Matrix[j][k] < D[k]:
                     D[k] = D[j] + Matrix[j][k]
+                    '''Update path data from start to new node. '''
                     if len(P[k]) != 0:
                         P[k] = []
                     if len(P[j]) != 0:
@@ -68,12 +82,13 @@ def BFAlgorithm(Matrix):
                     else:
                         P[k].append(vertexData[j])
                         P[k].append(vertexData[k])
-
+    '''This checks for any negative cycles. '''
     for i in range(7):
         for j in range(7):
             if D[i] + Matrix[i][j] < D[j]:
                 return False
-
+    for i in range(len(P)):
+        P[i].insert(0, 's')
     returnData.append(D)
     returnData.append(P)
     return returnData
