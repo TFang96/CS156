fed0ec0cda8ff7f863f712aaff5b272fd7081666
Tony Fang <78616310+TFang96@users.noreply.github.com>
2024-11-28 12:28:45 -0800

    Save code.
    Save progress.

--- Assignment9/Problem2.py
+++ Assignment9/Problem2.py
@@ -5,32 +5,49 @@ CS 156
 import pandas
 import math
 
-trainData = pandas.read_csv("ExampleTrainDataset.csv") ## read in data
+input_data = pandas.read_csv("ExampleTrainDataset.csv", usecols=["x1", "x2", "x3"]) #input data
+target_values = pandas.read_csv("ExampleTrainDataset.csv", usecols=["y"]) #outputs
 
 lambdaVal = 0.01 # learning rate
 
 iterations = 50
 
-## we were not given the original weights?
-w1old = 0
+#weights
+weights = []
+for i in range(len(input_data)):
+    weights.append(0)
 
-w2old = 0
+batch_size = len(input_data) # get batch size
 
-w3old = 0
+actual_outputs = []
 
-## we are not given the actual and estimated outputs
-y_actual = 0
+losses = []
 
-y_hat = 0
 
-def ActivationFunction(x):
+
+
+def activationfunc(x):
     return pow(math.e, x) / (1 + pow(math.e, x))
 
-def ActivationFunctionDeriv(x):
-    return ActivationFunction(x) * (1 - ActivationFunction(x))
+def activationfuncderiv(x):
+    return activationfunc(x) * (1 - activationfunc(x))
+
+def calculate_losses(y_hat):
+    sum = 0
+    for i in range(len(y_hat)):
+        sum += (target_values.iloc[i, 0] - y_hat[i]) ** 2
+    return sum
 
 def main():
     for i in range(iterations):
-        w1new = w1old - lambdaVal* 2(y_actual - y_hat) * ActivationFunctionDeriv(w1old)
-        w2new = w2old - lambdaVal* 2(y_actual - y_hat) * ActivationFunctionDeriv(w2old)
-        w3new = w3old - lambdaVal* 2(y_actual - y_hat) * ActivationFunctionDeriv(w3old)
+        linear_output = 0
+        for j in range(batch_size):
+            linear_output += input_data[j, 0] * weights[0] + input_data[j, 1] * weights[1] + input_data[j, 2] * weights[2] #linear output of each batch
+            y_hat = activationfunc(linear_output)
+            actual_outputs.append(y_hat) # this contains all the y_hat
+            losses.append(calculate_losses(y_hat))
+
+            #update weights using gradients
+            weights[0] -= lambdaVal * (-2*(target_values.iloc[j, 0] - y_hat) * activationfuncderiv(linear_output) * input_data.iloc[j, 0])
+            weights[1] -= lambdaVal * (-2*(target_values.iloc[j, 0] - y_hat) * activationfuncderiv(linear_output) * input_data.iloc[j, 1])
+            weights[2] -= lambdaVal * (-2*(target_values.iloc[j, 0] - y_hat) * activationfuncderiv(linear_output) * input_data.iloc[j, 2])
